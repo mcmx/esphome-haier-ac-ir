3ec6c0ac42d295ac5185e56180d8063ed8de2113
Sergio Guzman <sergio@sergioguzman.com>
2026-04-13 15:06:08 -0600

    change name in climate.py

--- components/haier_ac_ir/climate.py
+++ components/haier_ac_ir/climate.py
@@ -5,9 +5,9 @@ AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@mcmx"]
 
 haier_ac_ir_ns = cg.esphome_ns.namespace('haier_ac_ir')
-HaierACIRClimate = haier_ac_ir_ns.class_('HaierACIRClimate', climate_ir.ClimateIR)
+HaierIRClimate = haier_ac_ir_ns.class_('HaierIRClimate', climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HaierACIRClimate)
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HaierIRClimate)
 
 
 async def to_code(config):
