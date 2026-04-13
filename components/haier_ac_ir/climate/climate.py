29ba243a8bbb2c6b22f2e86ca963513ae0b1b21e
Sergio Guzman <sergio@sergioguzman.com>
2026-04-13 12:50:25 -0600

    Add SCHEMA to climate.py

--- components/haier_ac_ir/climate/climate.py
+++ components/haier_ac_ir/climate/climate.py
@@ -4,3 +4,9 @@ from esphome.components import climate_ir
 
 haier_ac_ir_ns = cg.esphome_ns.namespace('haier_ac_ir')
 HaierACIRClimate = haier_ac_ir_ns.class_('HaierACIRClimate', climate_ir.ClimateIR)
+
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HaierACIRClimate)
+
+
+async def to_code(config):
+    await climate_ir.new_climate_ir(config)
